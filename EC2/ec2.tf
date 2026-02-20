cadb05ac99578a63cf7b8926ee4cfc11bf749376
Reddisekhar Pamula <reddisekharpamula@gmail.com>
2026-02-20 12:27:46 +0530

    Create EC2 instance using terraform

---
+++ EC2/ec2.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "example" {
+    ami = "ami-0220d79f3f480ecf5"
+    instance_type = "t3.micro"
+    vpc_security_group_ids  = [aws_security_group.allow-tls.id]
+    tags = {
+      Name = "example-instance"
+      project = "Roboshop"
+    }
+  
+}
+
+resource "aws_security_group" "allow-tls" {
+ name = "allow-all-terraform"
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+    tags = {
+      Name = "allow-all-terraform"
+    }
+}
+
