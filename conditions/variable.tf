f8229c5e9cd67e07a01544fe2f0b5f34a0d0aa08
Reddisekhar Pamula <reddisekharpamula@gmail.com>
2026-02-20 17:38:47 +0530

    variabls and conditions practice

---
+++ conditions/variable.tf
@@ -0,0 +1,55 @@
+variable "ami_id" {
+    type = string
+    description = "AMI id of the instance"
+    default = "ami-0220d79f3f480ecf5"
+}
+
+variable "instance_type" {
+    type = string
+    description = "Instance type"
+    default = "t3.micro"
+}
+
+variable "ec2_tags" {
+    type = map
+    default = {
+        Name = "conditions-demo"
+        Project = "roboshop"
+        Terraform = "true"
+        Environment = "dev"
+    }
+}
+
+variable "sg_from_port" {
+    type = number
+    default = 0
+  
+}
+
+variable "sg_to_port" {
+    type = number
+    default = 0
+  
+}
+
+variable "sg_tags" {
+  type = map
+  default = {
+    Name = "allow-all-terraform"
+    Project = "roboshop"
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+variable "cidr_blocks" {
+  type = list
+  default = ["0.0.0.0/0"]
+}
+
+variable "environment" {
+    type = string
+    default = "prod"
+  
+}
+
